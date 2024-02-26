c95f718092709af2bc7c45b70f3081f177d4fe26
VeljkoStefanovic613-2019 <85968664+VeljkoStefanovic613-2019@users.noreply.github.com>
2024-02-26 14:53:50 +0100

    Custom Hook useFetch

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,18 +1,10 @@
-import { useEffect, useState } from "react";
+
 import { Card } from "../components";
-import { Await } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 
-export const MovieList = () => {
-const [movies, setMovies] = useState([]);
 
-useEffect(() => {
-  async function fetchMovies(){
-    const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=03ef7baef561f013e8bcbae1584f3fa5");
-    const data = await response.json();
-    setMovies(data.results);
-  }
-  fetchMovies();
-}, [] )
+export const MovieList = ({apiPath}) => {
+const {data: movies} = useFetch(apiPath)
 
   return (
     <main>
--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -5,11 +5,11 @@ export const AllRoutes = () => {
   return (
     <div className="dark:bg-slate-800">
         <Routes>
-            <Route path="/" element={<MovieList />} />    
+            <Route path="/" element={<MovieList apiPath="movie/now_playing" />} />    
             <Route path="movie/:id" element={<MovieDetail />} />    
-            <Route path="movies/popular" element={<MovieList />} />    
-            <Route path="movies/top" element={<MovieList />} />    
-            <Route path="movies/upcoming" element={<MovieList />} />    
+            <Route path="movies/popular" element={<MovieList apiPath="movie/popular" />} />    
+            <Route path="movies/top" element={<MovieList apiPath="movie/top_rated" />} />    
+            <Route path="movies/upcoming" element={<MovieList apiPath="movie/upcoming" />} />    
             <Route path="movies/search" element={<Search />} />    
             <Route path="*" element={<PageNotFound />} />    
         </Routes>  
