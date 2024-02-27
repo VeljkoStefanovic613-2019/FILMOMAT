a949247c624092b8a9e6d300275c3b9831bea4ba
VeljkoStefanovic613-2019 <85968664+VeljkoStefanovic613-2019@users.noreply.github.com>
2024-02-27 15:18:39 +0100

    small changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer>
         <footer className="bg-white  shadow dark:bg-gray-800">
             <div className="w-full mx-auto max-w-screen-2xl p-4 md:flex md:items-center md:justify-between">
-              <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link to="/" className="hover:underline">FILMOMAT</Link>. All Rights Reserved.
+              <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link to="/" className="hover:underline">FILMOMAT</Link>. All Rights Reserved.
             </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
