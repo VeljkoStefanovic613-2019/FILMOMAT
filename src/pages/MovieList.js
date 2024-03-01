8f6b3b563c13a095642eccc638f86206badaebf2
VeljkoStefanovic613-2019 <85968664+VeljkoStefanovic613-2019@users.noreply.github.com>
2024-03-01 11:32:54 +0100

    Tailwind CSS Customization

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -9,7 +9,7 @@ const {data: movies} = useFetch(apiPath);
   return (
     <main>
      <section className="max-w-7xl mx-auto py-7">
-      <div className="flex justify-start flex-wrap">
+      <div className="flex justify-start flex-wrap other:justify-evenly">
         {movies.map((movie) => (
             <Card key={movie.id} movie={movie} />
         ))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "other": {'min': '340px', 'max': '1200px'},
+      },
+      colors: {
+        darkbg: "#1E293B",
+      }
+    },
   },
   plugins: [],
 }
