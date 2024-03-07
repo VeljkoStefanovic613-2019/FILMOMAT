043c1ed95a06e1c09b826dacd8048905360d2685
VeljkoStefanovic613-2019 <85968664+VeljkoStefanovic613-2019@users.noreply.github.com>
2024-03-07 03:09:13 +0100

    ScrollToTop functionality

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ export { Header } from "./Header";
 export { Footer } from "./Footer";
 
 export { Card } from "./Card";
-
 export { Button } from "./Button";
+
+export { ScrollToTop } from "./ScrollToTop";
